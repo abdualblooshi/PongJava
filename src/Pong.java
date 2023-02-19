006354e29fed6f7a15b46b201889b01e0150d723
Abdulrahman Alblooshi <aboodshadow@outlook.com>
2023-02-19 18:10:43 +0400

    Imported JavaFX

---
+++ src/Pong.java
@@ -0,0 +1,21 @@
+import java.util.Random;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.TextAlignment;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+
+public class Pong{
+    public static void main(String[] args){
+
+
+    }
+}
